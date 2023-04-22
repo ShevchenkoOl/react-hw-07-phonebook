48f0a85a405a85755a4e81a1b928491481cb2d83
Oleksii Shevchenko <uzlabini@gmail.com>
2023-04-22 12:25:44 +0200

    858

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+
+import ContactForm from "./ContactForm/ContactForm";
+import { Container, Title } from "./ContactForm/ContactForm.styled";
+import  ContactList  from "./ContactList/ContactList";
+import { Filter } from "./Fiter/Filter";
+import { GlobalStyle } from "./GlobalStyle";
+
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+    return (
+
+      <Container>
+            <ContactForm title="Phonebook"/>
+         <Title>Contacts
+            <Filter />
+            <ContactList title="Contacts" />
+          </Title>
+          <GlobalStyle/>
+      </Container>        
+    );
+}
