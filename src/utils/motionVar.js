7853755102d58cc3a960b5ebda27778b92d14371
Oleksii Shevchenko <uzlabini@gmail.com>
2023-04-22 17:38:58 +0200

    7778

--- src/utils/motionVar.js
+++ src/utils/motionVar.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:4040';
+axios.defaults.baseURL = 'http://localhost:3000';
 
 export function fetchContacts() {
   return axios.get('/contacts');
