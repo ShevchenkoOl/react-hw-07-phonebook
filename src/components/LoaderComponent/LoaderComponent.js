1044f8c7bd732b3da4e6f47461e6002a59b72057
Oleksii Shevchenko <uzlabini@gmail.com>
2023-04-22 15:02:26 +0200

    23

--- src/components/LoaderComponent/LoaderComponent.js
+++ src/components/LoaderComponent/LoaderComponent.js
@@ -1,5 +1,5 @@
 import Loader from 'react-loader-spinner';
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+//import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import {Overlay} from './LoaderComponent.styled';
 
 function LoaderComponent() {
