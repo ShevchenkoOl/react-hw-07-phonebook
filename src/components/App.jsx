d3f7c493595ee7326430cb94ed532831f03fe694
Oleksii Shevchenko <uzlabini@gmail.com>
2023-04-22 13:33:18 +0200

    787

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,16 @@
-
-import ContactForm from "./ContactForm/ContactForm";
-import { Container, Title } from "./ContactForm/ContactForm.styled";
-import  ContactList  from "./ContactList/ContactList";
-import { Filter } from "./Fiter/Filter";
-import { GlobalStyle } from "./GlobalStyle";
-
-
 export const App = () => {
-    return (
-
-      <Container>
-            <ContactForm title="Phonebook"/>
-         <Title>Contacts
-            <Filter />
-            <ContactList title="Contacts" />
-          </Title>
-          <GlobalStyle/>
-      </Container>        
-    );
-}
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import contactsReducer from './contacts/contacts-reduce';
-
-export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-  },
-});
