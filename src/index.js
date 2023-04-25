8ae761d4f8fd3458dc29da2ca1c61729ee0938b8
Oleksii Shevchenko <uzlabini@gmail.com>
2023-04-25 22:49:03 +0200

    8585

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
 import './components/GlobalStyle';
 import { Provider } from 'react-redux';
-import { store} from './redux/store' 
-//import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
         <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,48 @@
-import { configureStore } from '@reduxjs/toolkit';
+// import { configureStore } from '@reduxjs/toolkit';
+// import contactsReducer from './contacts/contacts-reduce';
+
+// export const store = configureStore({
+//   reducer: {
+//     contacts: contactsReducer,
+//   },
+// });
+import { configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
+//import logger from 'redux-logger';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import contactsReducer from './contacts/contacts-reduce';
+import { logger } from 'redux-logger';
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
+];
+
+const contactsPersistConfig = {
+  key: 'contacts',
+  storage,
+  blacklist: ['filter'],
+};
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistReducer(contactsPersistConfig, contactsReducer),
   },
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
